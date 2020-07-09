f255077b86e98b372eb4f21e063efa32831c8f79
MattKrabacher <matt.krabacher@leanpath.com>
2020-07-09 23:08:16 +0000

    add code in files types: .md,.kts,.java,.sql remove code in files types: .md,.kts,.java,.sql

--- content.java
+++ content.java
@@ -10,6 +10,6 @@ public class Cvjoqt {
         System.out.println("spaig");
         System.out.println("sphou");
         System.out.println("vrdci");
-        System.out.println("rlyhu");
+        System.out.println("iagty");
     }
 }
