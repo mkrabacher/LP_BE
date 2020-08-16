ce539de61ecdb96d7101357d9cc8b82bf7bff026
MattKrabacher <matt.krabacher@leanpath.com>
2020-08-16 16:21:22 +0000

    add code in files types: .kt,.java remove code in files types: .kt,.java

--- content.java
+++ content.java
@@ -13,6 +13,6 @@ public class Cvjoqt {
         System.out.println("cimwt");
         System.out.println("vdaou");
         System.out.println("okhhs");
-        System.out.println("ukedi");
+        System.out.println("slhgr");
     }
 }
