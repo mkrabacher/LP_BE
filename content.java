5459d162b278bd772cdb7896325736d627defd07
MattKrabacher <matt.krabacher@leanpath.com>
2021-05-27 18:15:29 +0000

    add code in files types: remove code in files types: .java,.properties,.html

--- content.java
+++ content.java
@@ -22,6 +22,5 @@ public class Cvjoqt {
         System.out.println("bsasp");
         System.out.println("veflp");
         System.out.println("eiadn");
-        System.out.println("wwlys");
     }
 }
