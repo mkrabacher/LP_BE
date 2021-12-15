6755b4cbfec110750dd47d284ff36d131348fdfe
MattKrabacher <matt.krabacher@leanpath.com>
2021-12-15 22:40:42 +0000

    add code in files types: ,.kts,.xml,.properties,.java,.md remove code in files types: ,.xml,.properties,.kts,.java

--- content.java
+++ content.java
@@ -446,6 +446,6 @@ public class Cvjoqt {
         System.out.println("lqjwa");
         System.out.println("xtrqn");
         System.out.println("cjtup");
-        System.out.println("vqrje");
+        System.out.println("xabgk");
     }
 }
