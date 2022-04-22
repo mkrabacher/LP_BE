613a34e1c2042651d914a864d8d4671bf29a8234
Matt Krabacher <matt.krabacher@leanpath.com>
2022-04-22 15:46:17 +0000

    add code in files types: .kt,.java,.csv remove code in files types: .kt,.java

--- content.java
+++ content.java
@@ -649,6 +649,6 @@ public class Cvjoqt {
         System.out.println("wyvto");
         System.out.println("vxrpw");
         System.out.println("mdanj");
-        System.out.println("inhov");
+        System.out.println("wqeni");
     }
 }
