90fe71116c74310bc0eec8e525e914d62962c8fc
Matt Krabacher <matt.krabacher@leanpath.com>
2022-07-14 14:39:25 +0000

    add code in files types: .kt,.sql,.java remove code in files types: .java

--- content.java
+++ content.java
@@ -713,6 +713,6 @@ public class Cvjoqt {
         System.out.println("ywvfm");
         System.out.println("itfap");
         System.out.println("djgos");
-        System.out.println("xabqa");
+        System.out.println("gwuxy");
     }
 }
