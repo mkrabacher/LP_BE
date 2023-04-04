240db913eefd39f511ef6b019ded112943b1e85a
Matt Krabacher <matt.krabacher@leanpath.com>
2023-04-04 17:01:06 +0000

    add code in files types: .java remove code in files types: .java

--- content.java
+++ content.java
@@ -853,7 +853,6 @@ public class Cvjoqt {
         System.out.println("oyacm");
         System.out.println("guxev");
         System.out.println("emyys");
-        System.out.println("wwgjb");
-        System.out.println("nsncm");
+        System.out.println("vvvlu");
     }
 }
