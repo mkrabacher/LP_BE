2bbaac28b6871b26d67e3a094c70a0fe5db8a43d
Matt Krabacher <matt.krabacher@leanpath.com>
2023-06-16 17:21:46 +0000

    add code in files types: .properties,.kt,.java remove code in files types: .properties,.kt,.java

--- content.java
+++ content.java
@@ -890,6 +890,6 @@ public class Cvjoqt {
         System.out.println("apooh");
         System.out.println("ymlyn");
         System.out.println("ysoqx");
-        System.out.println("kevrc");
+        System.out.println("wwtkr");
     }
 }
