45368b45a6eeea0dec1018d6c13f01837850b90c
Matt Krabacher <matt.krabacher@leanpath.com>
2023-09-23 00:18:24 +0000

    add code in files types: .1__DynamicMenuGalleyAdapterLocationConfigurationUpdate,.kts,.java remove code in files types: .kts,.java

--- content.java
+++ content.java
@@ -957,6 +957,6 @@ public class Cvjoqt {
         System.out.println("iueav");
         System.out.println("gouaa");
         System.out.println("pjhod");
-        System.out.println("ceojk");
+        System.out.println("gqdef");
     }
 }
